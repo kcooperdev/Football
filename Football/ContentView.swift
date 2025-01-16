f169fa1dced1c8043b47a1bd4c4e8877daadca89
kcooperdev <khalif.cooper@outlook.com>
2025-01-16 08:29:50 -0500

    update after first lesson

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is Football To You ?")
+                .foregroundStyle(.green)
+                .font(.largeTitle)
+                .fontWeight(.thin)
+            }
+        HStack {
+            Image(systemName:"figure.american.football")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.blue)
+            Image(systemName: "figure.australian.football")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.indigo)
+            Image(systemName: "figure.soccer")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.purple)
+            
         }
         .padding()
     }
